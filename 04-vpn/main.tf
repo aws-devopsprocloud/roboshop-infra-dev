96a48d9f1a58db867bc31e645836d97b80c6e65e
mac-devopsprocloud <premsagar.eri@devopsprocloud.in>
2026-05-25 23:28:34 +0530

    roboshop-infra-dev-jenkins

---
+++ 04-vpn/main.tf
@@ -0,0 +1,18 @@
+module "vpn" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  ami = data.aws_ami.openvpn.id
+  name = "${var.project}-${var.environment}-vpn"
+
+  instance_type = "t3.micro"
+  subnet_id     = local.public_subnet_id
+  vpc_security_group_ids = [data.aws_ssm_parameter.vpn_sg_id.value]
+  create_security_group = false
+  user_data = file("vpn.sh")
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+    Name = "${var.project}-${var.environment}-vpn"
+  }
+}
+
