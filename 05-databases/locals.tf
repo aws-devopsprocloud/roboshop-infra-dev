413590a08182018cafb3ffbb810d97b9c0cb9ad7
mac-devopsprocloud <premsagar.eri@devopsprocloud.in>
2026-05-26 12:29:51 +0530

    roboshop-infra-dev-jenkis

--- 05-databases/locals.tf
+++ 05-databases/locals.tf
@@ -1,5 +1,4 @@
 locals {
   database_subnet_id = element(split(",", data.aws_ssm_parameter.database_subnet_ids.value), 0)
-  default_vpc_public_subnet_id = data.aws_subnets.default.ids[0]
   # we are splitting the subnets id's and taking first element
 }
