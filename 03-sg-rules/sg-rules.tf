5afe470df3599de22a25593e0fe17677f33b51d2
mac-devopsprocloud <premsagar.eri@devopsprocloud.in>
2026-05-30 09:05:28 +0530

    roboshop-infra-dev-jenkis

--- 03-sg-rules/sg-rules.tf
+++ 03-sg-rules/sg-rules.tf
@@ -868,17 +868,27 @@ resource "aws_security_group_rule" "payment_backend_alb" {
   security_group_id = data.aws_ssm_parameter.payment_sg_id.value
 }
 
-resource "aws_security_group_rule" "frontend_vpn" {
+# resource "aws_security_group_rule" "frontend_vpn" {
+#   type              = "ingress"
+#   from_port         = 22
+#   to_port           = 22
+#   protocol          = "tcp"
+#   #cidr_blocks       = ["0.0.0.0/0"]
+#   source_security_group_id = data.aws_ssm_parameter.vpn_sg_id.value
+#   # which SG you are creating this rule
+#   security_group_id = data.aws_ssm_parameter.frontend_sg_id.value
+# }
+
+resource "aws_security_group_rule" "frontend_bastion" {
   type              = "ingress"
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
   #cidr_blocks       = ["0.0.0.0/0"]
-  source_security_group_id = data.aws_ssm_parameter.vpn_sg_id.value
+  source_security_group_id = data.aws_ssm_parameter.bastion_sg_id.value
   # which SG you are creating this rule
   security_group_id = data.aws_ssm_parameter.frontend_sg_id.value
 }
-
 
 resource "aws_security_group_rule" "frontend_alb_internet" {
   type              = "ingress"
